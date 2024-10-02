5a3a26ae34efc5d42460115f606685d1d0b6fd31
alimoncul <alimoncul@gmail.com>
2024-10-02 21:19:21 +0300

    feature: add body logging

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -6,13 +6,16 @@ export class LoggerMiddleware implements NestMiddleware {
   private readonly logger = new Logger('HTTP');
 
   use(req: Request, res: Response, next: NextFunction): void {
-    const { method, originalUrl } = req;
+    const { method, originalUrl, body } = req;
     const start = Date.now();
+    const bodyHasData = Object.keys(body).length > 0;
 
     res.on('finish', () => {
       const { statusCode } = res;
       const duration = Date.now() - start;
-      this.logger.log(`${method} ${originalUrl} ${statusCode} - ${duration}ms`);
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} - ${duration}ms${bodyHasData ? ` - Body: ${JSON.stringify(body)}` : ``}`,
+      );
     });
 
     next();
